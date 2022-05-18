8a350a48fcfb99634e049156f197f82e2c5669b8
izulolov <izulolov@mail.ru>
2022-05-18 17:34:16 +0300

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
-
+  
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  #Именнованный маршрут
   get 'signup'  => 'users#new'
+  
+  #Неименнованный маршрут
+  #get 'users#new'
 end
