f975c5e9f23761218343db11e1e3d4cd7160c462
izulolov <izulolov@mail.ru>
2022-05-25 12:01:24 +0300

    Finish log in/log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,13 +14,13 @@ module SessionsHelper
       # Возвращает пользователя, соответствующего remember-токену в куки.
     def current_user
         if (user_id = session[:user_id])
-        @current_user ||= User.find_by(id: user_id)
+            @current_user ||= User.find_by(id: user_id)
         elsif (user_id = cookies.signed[:user_id])
-        user = User.find_by(id: user_id)
-        if user && user.authenticated?(cookies[:remember_token])
-            log_in user
-            @current_user = user
-        end
+            user = User.find_by(id: user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+                log_in user
+                @current_user = user
+            end
         end
     end
    
@@ -28,18 +28,18 @@ module SessionsHelper
     def logged_in?
         !current_user.nil?
     end
-
-    def log_out
-        session.delete(:user_id)
-        @current_user = nil
-    end
-
-    # Забывает пользователя
     # Забывает постоянную сессии.
     def forget(user)
         user.forget
         cookies.delete(:user_id)
         cookies.delete(:remember_token)
+    end
+
+    # Осуществляет выход текущего пользователя.
+    def log_out
+        forget(current_user)
+        session.delete(:user_id)
+        @current_user = nil
     end
 
 
